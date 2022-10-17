5d929916e82e6dd6ffe7e54f8efc435d096ac234
GillesFromGuinot <glevy@guinot.asso.fr>
2022-10-17 15:25:53 +0200

    Lorem ipsum

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController extends AbstractController
+    class ArticleController extends AbstractController
 {
     /**
      * @Route("/article", name="app_article")
@@ -17,4 +17,25 @@ class ArticleController extends AbstractController
             'controller_name' => 'ArticleController',
         ]);
     }
+
+
+    /**
+     * @Route("/catalogue", name="app_catalogue")
+     */
+    public function catalogue(): Response
+    {
+        return $this->render('article/catalogue.html.twig', [
+
+        ]);
+    }
+    /**
+     * @Route("/abonnements", name="app_abonnements")
+     */
+    public function abonnements(): Response
+    {
+        return $this->render('article/abonnement.html.twig', [
+
+        ]);
+    }
+
 }
