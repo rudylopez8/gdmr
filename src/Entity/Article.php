4c540c93e7a2ac157ce1e56862191b876d823466
GillesFromGuinot <glevy@guinot.asso.fr>
2022-10-19 14:47:17 +0200

    titre of my entity

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -17,8 +17,25 @@ class Article
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
     }
 }
