09652fb11830c8e2def0cdec76cba682f613e944
rudylopez8 <rudy.lopez.houplin@gmail.com>
2022-11-03 15:08:16 +0100

    clef.auteur.active

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -4,20 +4,40 @@ namespace App\DataFixtures;
 
 use App\Entity\Article;
 use App\Entity\Categorie;
+use App\Entity\User;
 
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use PhpParser\Node\Expr\New_;
-
+use Doctrine\Common\Collections\ArrayCollection;
 
 class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
 
     {
+$tabAuteur=new ArrayCollection();
         // Utiliser la Bibliotheque Fakar
         $faker = \Faker\Factory::create('fr_FR');
+        for ($i=0; $i<100; $i++){
+            $user = new User();
+            $user->setNom($faker->lastName)
+                    ->setPrenom($faker->firstName())
+                    ->setPseudo($faker->word())
+                    ->setPhoto($faker->imageUrl(350, 350))
+                    ->setAdresse($faker->streetAddress())
+                    ->setCodePostal($faker->postcode())
+                    
+                    ->setVille($faker->city())
+                    ->setPays($faker->country())
+                    ->setTelephone($faker->phoneNumber())
+                    ->setMail($faker->email());
+            $tabAuteur->add($user);
+                $manager->persist($user);
+        }
+
+
 
          // je mets en place 5 categories
          for ($i=0; $i<2; $i++){
@@ -31,7 +51,7 @@ class ArticleFixtures extends Fixture
         for ($i=0; $i<12; $i++){
             $article = new Article();
             $article->setTitre($faker->sentence($nb = 5, $asText = false))
-                    ->setAuteur($faker->name)
+                    ->setAuteur($tabAuteur->get(mt_rand(1,count($tabAuteur))))
                     ->setImage($faker->imageUrl(350, 350))
                     ->setResume($faker->text())
                     ->setContenu($faker->realText($maxNbChars = 200, $indexSize = 2))
